777a2081af744f628b6d22c1525939448b6e19d3
Aryan Mane <aryanmane7916@gmail.com>
2025-09-22 23:44:04 +0530

    feat: added font matter and applied it globally

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,12 @@
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Next.js!</h1>
+      <div className="min-h-screen flex items-center justify-center text-black transition-all duration-300">
+        <div className="max-w-3xl text-center space-y-10">
+          <h1 className="text-6xl font-semibold">Chaze</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
     </main>
   );
 }
